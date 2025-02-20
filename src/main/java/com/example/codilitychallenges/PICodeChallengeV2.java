201ff109bbac6a6701ecfd2e289fd82f2e8af9cd
George Kankava <gkankava@uplandsoftware.com>
2025-02-20 11:02:14 +0400

    Pi Challenge V2

--- src/main/java/com/example/codilitychallenges/PICodeChallengeV2.java
+++ src/main/java/com/example/codilitychallenges/PICodeChallengeV2.java
@@ -1,9 +1,11 @@
 package com.example.codilitychallenges;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -49,28 +51,59 @@ public class PICodeChallengeV2 {
             index++;
         }
 
+        List<Character> characterLeft = freqMap.keySet()
+                .stream()
+                .filter(e1 -> Arrays.stream(charsToConsiderArray).noneMatch(e2 -> e2 == e1)).collect(Collectors.toList());
+
+
+        Character [] charsToConsiderArrayReversed = Arrays.stream(charsToConsiderArray).sorted(Comparator.reverseOrder()).distinct().toArray(Character[]::new);
+
         Character [] result = new Character[P.length()];
-        Arrays.fill(result, '$');
+        Character [] resultWithReversed = new Character[P.length()];
 
         // filled positions in result
         Set<Integer> filledPositions = new HashSet<>();
+        Set<Integer> filledPositionsForReversed = new HashSet<>();
         for (int i = 0; i < charsToConsider; i++) {
             for (int j = 0; j < P.length(); j++) {
                 if ((charsToConsiderArray[i] == P.charAt(j) || charsToConsiderArray[i] == Q.charAt(j)) && (!filledPositions.contains(j))) {
                     result[j] = charsToConsiderArray[i];
                     filledPositions.add(j);
                 }
+                if ((charsToConsiderArrayReversed[i] == P.charAt(j) || charsToConsiderArrayReversed[i] == Q.charAt(j)) && (!filledPositionsForReversed.contains(j))) {
+                    resultWithReversed[j] = charsToConsiderArrayReversed[i];
+                    filledPositionsForReversed.add(j);
+                }
             }
         }
-        return Arrays.stream(result).collect(Collectors.toSet()).size();
+        for (int i = 0; i < P.length(); i++) {
+            if (result[i] == null) {
+                for (Character c : characterLeft) {
+                    if (c.equals(P.charAt(i)) || c.equals(Q.charAt(i))) {
+                        result[i] = c;
+                        resultWithReversed[i] = c;
+                    }
+                }
+            }
+        }
+
+
+
+        int size = Arrays.stream(result).filter(Objects::nonNull).collect(Collectors.toSet()).size();
+        int sizeOfReversed = Arrays.stream(resultWithReversed).filter(Objects::nonNull).collect(Collectors.toSet()).size();
+        return Math.min(size, sizeOfReversed);
     }
 
     // bacad abada
     // hsuledusla jfudisoala
     // axxz yzwy
-    // axxz yzwy
+    // axxz wzyy
     // "adabca", "cbdcdb"
+    // "dcba" "cbad"
+    // "ad" "bc"
+    // acb cba
+    // awwa bbwx
     public static void main(String[] args) {
-        System.out.println(solution("adabca", "cbdcdb"));// ccaa
+        System.out.println(solution("dcba", "cbad"));// ccaa
     }
 }
